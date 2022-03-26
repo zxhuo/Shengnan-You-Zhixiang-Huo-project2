a2dc04b675c67049fd93afafb8d03d20a6ddb523
Oliver Huo <oliverhuo@Olivers-MacBook-Pro.local>
2022-03-25 19:21:45 -0700

    Added Rule

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet, Link, useParams } from 'react-router-dom';
 import { Fragment } from 'react';
+import Rule from './Rule'
 
 function Navigation() {
   let { level  } = useParams();
   let path = '/games/' + level;
+
   return (
     <Fragment>
         <div className='ui secondary pointing menu'>
@@ -12,12 +14,8 @@ function Navigation() {
             <div className='item'>Wordle</div>
           </Link>
           <div className='right menu'>
-            <Link className='item' to='/rule'>
-              RULE
-            </Link>
-           
-          
-          </div>
+            <Rule />
+            </div>
         </div>
         <Outlet />
     </Fragment>
--- src/components/Rule.js
+++ src/components/Rule.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Fragment, useState } from 'react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
-function Rule() {
+
+function Rule(props) {
+
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-        <h1>this is rule page</h1>
-        A page with the rules for the game
-    </div>
+    <Modal
+    basic
+    onClose={() => setOpen(false)}
+    onOpen={() => setOpen(true)}
+    open={open}
+    size='small'
+    trigger={<Button>Rule</Button>}
+  >
+    <Header>
+      HOW TO PLAY
+    </Header>
+
+    <Modal.Content>
+      <div className='instructions'>
+        <p>Guess the WORDLE in seven tries.</p>
+        <p>Each guess must be a valid five-letter word. Hit the enter button to submit.</p>
+        <p>After each guess, the color of the tiles will change to show how close your guess was to the word.</p>
+
+        <div className='examples'>
+          <p><strong>EXAMPLE</strong></p>
+        </div>
+      </div>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button basic color='red' inverted onClick={() => setOpen(false)}>
+        <Icon name='remove' /> Close
+      </Button>
+    </Modal.Actions>
+  </Modal>
   )
 }
 
