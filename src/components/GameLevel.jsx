049af51a02e50f282c3829e64361a2df06d24296
Oliver Huo <oliverhuo@Olivers-MacBook-Pro.local>
2022-03-28 18:14:06 -0700

    updated game level

--- src/components/GameLevel.jsx
+++ src/components/GameLevel.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-
 import { GameProvider } from '../contexts/GameProvider'
 import Game from "./games/Game";
 function GameLevel() {
@@ -8,13 +7,13 @@ function GameLevel() {
     let trytime;
     if (level === 'hard') {
         len = 7;
-        trytime = 2;
+        trytime = 5;
     } else if (level === 'medium') {
         len = 6;
-        trytime = 2;
+        trytime = 6;
     }else {
         len = 5;
-        trytime = 2;
+        trytime = 7;
     }
     return (
             <GameProvider len={len} trytime={trytime} >
